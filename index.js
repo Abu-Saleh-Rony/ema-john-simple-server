523f37acd6b9fa455ebb4bbc06b67a49cfe2fb93
abusalehrony <abusalehrony12@gmail.com>
2022-11-08 13:03:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { response } = require('express');
+require('dotenv').config();
+const app = express();
+const port = process.env.port || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xtwoonc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        const productsCollection = client.db('emaJohn').collection('products');
+
+        app.get('/products', async (req, res) => {
+
+            const query = {}
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            const count = await productsCollection.estimatedDocumentCount();
+            res.send({ count, products });
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
+
+app.get('/', (req, res) => {
+    res.send('Ema John is Running')
+});
+
+
+
+app.listen(port, () => {
+    console.log(`ema john running on: ${port}`)
+});
